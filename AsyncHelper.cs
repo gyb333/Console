bdd1a266b3011b615f214de6dd7e2cd529788a17
gyb333 <876368840@qq.com>
2015-10-09 15:46:15 +0800

    add

---
+++ AsyncHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class AsyncHelper
+    {
+
+
+        public void AsyncByHandler()
+        {
+            Action<object> action = (obj) => AsyncFunc(obj);
+            string args = string.Empty;
+            //action.BeginInvoke(args, ar => action.EndInvoke(ar), null);
+            IAsyncResult result = action.BeginInvoke(args, CallBack, null);
+            action.EndInvoke(result);
+        }
+
+        public object AsyncFunc(object args)
+        {
+            return null;
+        }
+
+         void CallBack(IAsyncResult result)
+        {
+            AsyncResult handler = result as AsyncResult;
+           
+
+        }
+
+    }
+}
